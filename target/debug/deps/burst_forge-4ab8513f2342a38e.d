8267e715f8b67a4b420899d6d6c0d33287e96b06
drzln <luis.d@akeyless.io>
2026-03-30 13:33:03 -0400

    add .gitignore, exclude target/

--- target/debug/deps/burst_forge-4ab8513f2342a38e.d
+++
@@ -1,5 +0,0 @@
-/Users/luis.d/code/github/pleme-io/burst-forge/target/debug/deps/burst_forge-4ab8513f2342a38e.d: src/main.rs
-
-/Users/luis.d/code/github/pleme-io/burst-forge/target/debug/deps/libburst_forge-4ab8513f2342a38e.rmeta: src/main.rs
-
-src/main.rs:
